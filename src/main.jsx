db36cddc463ef45a204860ec5646cee00c0d4453
WadieChantir <wadii.chantir@effyis-partners.ma>
2026-07-13 22:38:27 +0100

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./styles/global.css";
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
     <HelmetProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </HelmetProvider>
   </StrictMode>
 );
