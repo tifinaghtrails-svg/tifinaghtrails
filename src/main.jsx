1ce7f9fd33fc9d298a44a33fa7f79f52aa5b5a17
WadieChantir <wadii.chantir@effyis-partners.ma>
2026-07-13 22:34:20 +0100

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./styles/global.css";
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </StrictMode>
 );
